b54a28cec519eb3dc1031ecbb7484a6329c78c8d
Kristin Brewer <kr527106@gmail.com>
2023-04-05 13:41:12 -0400

    added queries.js to utils in client/public

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+// import React and Apollo Client
+import { gql } from '@apollo/client';
+
+// execute me query 
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
